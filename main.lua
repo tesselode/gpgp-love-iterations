f576246e7e06cc0f8f0e627067ec158a728a0960
tesselode <aminnich3@gmail.com>
2015-11-16 18:36:13 -0500

    Object palette button arrangement

--- main.lua
+++ main.lua
@@ -3,6 +3,15 @@ Gamestate = require 'lib.gamestate'
 ObjectPalette = require 'state.object-palette'
 
 function love.load()
+  --load project
+  entities = {}
+  for _, entity in pairs(love.filesystem.load('project/entities.lua')()) do
+    table.insert(entities, {
+      name = entity.name,
+      image = love.graphics.newImage('project/images/'..entity.image)
+    })
+  end
+
   Gamestate.switch(ObjectPalette)
   Gamestate.registerEvents()
 end
--- state/object-palette.lua
+++ state/object-palette.lua
@@ -1,17 +1,72 @@
+local bSize    = 100
+local bSpace = 1.5
+
+
+
 local Button = require 'ui.button'
+
+local EntityButton = Button:extend()
+
+function EntityButton:new(x, y, w, h, entity)
+  EntityButton.super.new(self, x, y, w, h)
+  self.entity = entity
+end
+
+function EntityButton:draw()
+  EntityButton.super.draw(self)
+end
+
+
 
 local ObjectPalette = {}
 
 function ObjectPalette:enter()
-  self.button = Button(10, 10, 300, 100)
+  self:generateButtons()
+end
+
+function ObjectPalette:add(x, y, entity)
+  table.insert(self.buttons, EntityButton(x, y, bSize, bSize, entity))
+end
+
+function ObjectPalette:generateButtons()
+  local lg = love.graphics
+
+  self.buttons = {}
+
+  local gridWidth          = math.floor(lg.getWidth() / (bSize * bSpace))
+  local buttonTotalWidth   = gridWidth * bSize
+  local buttonSpacingWidth = (gridWidth - 1) * bSize * (bSpace - 1)
+  local totalWidth         = buttonTotalWidth + buttonSpacingWidth
+  local offsetX            = (lg.getWidth() - totalWidth) / 2
+  local offsetY            = 100
+
+  local gridX, gridY = 0, 0
+  for _, entity in pairs(entities) do
+    if gridX == gridWidth then
+      gridY = gridY + 1
+      gridX = 0
+    end
+    local x = offsetX + gridX * bSize * bSpace
+    local y = offsetY + gridY * bSize * bSpace
+    self:add(x, y, entity)
+    gridX = gridX + 1
+  end
+end
+
+function ObjectPalette:resize()
+  self:generateButtons()
 end
 
 function ObjectPalette:update(dt)
-  self.button:update(dt)
+  for _, button in pairs(self.buttons) do
+    button:update(dt)
+  end
 end
 
 function ObjectPalette:draw()
-  self.button:draw()
+  for _, button in pairs(self.buttons) do
+    button:draw()
+  end
 end
 
 return ObjectPalette
