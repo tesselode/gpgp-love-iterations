e5589218a5b8e38fb4b4c34d65b6d969ad84bc7d
tesselode <aminnich3@gmail.com>
2015-11-17 01:57:07 -0500

    Smoother zooming

--- state/editor.lua
+++ state/editor.lua
@@ -1,11 +1,19 @@
+local lg = love.graphics
+
+local function lerp(a, b, x)
+  return a + (b - a) * x
+end
+
 local Editor = {}
 
 function Editor:enter()
   self.mousePos  = Vector()
   self.mousePrev = Vector()
 
-  self.pan   = Vector()
-  self.scale = 25
+  self.pan          = Vector()
+  self.scale        = 5
+  self.displayPan   = self.pan
+  self.displayScale = self.scale
 end
 
 function Editor:update(dt)
@@ -16,44 +24,50 @@ function Editor:update(dt)
 
   --panning
   if love.mouse.isDown 'm' then
-    self.pan = self.pan + mouseDelta
+    self.pan        = self.pan + mouseDelta
+    self.displayPan = self.pan
   end
+
+  self.displayPan.x = lerp(self.displayPan.x, self.pan.x, 1 - (10^-5) ^ dt)
+  self.displayPan.y = lerp(self.displayPan.y, self.pan.y, 1 - (10^-5) ^ dt)
+  self.displayScale = lerp(self.displayScale, self.scale, 1 - (10^-5) ^ dt)
 end
 
 function Editor:mousepressed(x, y, button)
   if button == 'wu' then
     self.scale = self.scale * 1.1
-    self.pan   = self.pan * 1.1
   end
   if button == 'wd' then
     self.scale = self.scale / 1.1
-    self.pan   = self.pan / 1.1
   end
 end
 
 function Editor:draw()
-  love.graphics.push()
-  love.graphics.translate(self.pan.x, self.pan.y)
-  love.graphics.scale(self.scale)
-    love.graphics.setColor(255, 255, 255)
-    love.graphics.setLineWidth(1 / self.scale)
+  lg.push()
+  lg.translate(lg.getWidth() / 2, lg.getHeight() / 2)
+  lg.translate(self.displayPan.x, self.displayPan.y)
+  lg.scale(self.displayScale)
+  lg.translate(-level.width / 2, -level.height / 2)
 
-    --draw border
-    love.graphics.line(0, 0, level.width, 0)
-    love.graphics.line(0, level.height, level.width, level.height)
-    love.graphics.line(0, 0, 0, level.height)
-    love.graphics.line(level.width, 0, level.width, level.height)
+  love.graphics.setLineWidth(1 / self.displayScale)
 
-    --draw gridlines
-    love.graphics.setColor(255, 255, 255, 100)
-    for i = 1, level.width - 1 do
-      love.graphics.line(i, 0, i, level.height)
-    end
-    for i = 1, level.height - 1 do
-      love.graphics.line(0, i, level.width, i)
-    end
+  --draw border
+  love.graphics.setColor(255, 255, 255)
+  lg.line(0, 0, level.width, 0)
+  lg.line(0, level.height, level.width, level.height)
+  lg.line(0, 0, 0, level.height)
+  lg.line(level.width, 0, level.width, level.height)
 
-  love.graphics.pop()
+  --draw gridlines
+  lg.setColor(255, 255, 255, 100)
+  for i = 1, level.width - 1 do
+    lg.line(i, 0, i, level.height)
+  end
+  for i = 1, level.height - 1 do
+    lg.line(0, i, level.width, i)
+  end
+
+  lg.pop()
 end
 
 return Editor
