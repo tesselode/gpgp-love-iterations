a8bde58c795dd6ea78305770b8300491d034a404
tesselode <aminnich3@gmail.com>
2015-11-16 22:38:18 -0500

    Unfinished group/layer picker

--- state/layer-picker.lua
+++ state/layer-picker.lua
@@ -6,21 +6,31 @@ local MenuOption = require 'ui.menu-option'
 local LayerPicker = {}
 
 function LayerPicker:enter()
-  self.scrollArea = ScrollArea(0, 0, lg.getWidth() / 2 - 10, lg.getHeight())
+  self.groupMenu = ScrollArea(10, 10, lg.getWidth() / 2 - 40, lg.getHeight())
+  for n, group in pairs(level.groups) do
+    local y = self.groupMenu.contentHeight
+    local menuOption = MenuOption(0, y, self.groupMenu.w, group.name)
+    self.groupMenu:add(menuOption)
+    self.groupMenu:expand(menuOption.h)
+  end
+
+  self.layerMenu = ScrollArea(lg.getWidth() / 2 + 10, 10, lg.getWidth() / 2 - 40, lg.getHeight())
   for n, layer in pairs(level.groups[1].layers) do
-    local y = self.scrollArea.contentHeight
-    local menuOption = MenuOption(0, y, self.scrollArea.w, layer.name)
-    self.scrollArea:add(menuOption)
-    self.scrollArea:expand(menuOption.h)
+    local y = self.layerMenu.contentHeight
+    local menuOption = MenuOption(0, y, self.layerMenu.w, layer.name)
+    self.layerMenu:add(menuOption)
+    self.layerMenu:expand(menuOption.h)
   end
 end
 
 function LayerPicker:update(dt)
-  self.scrollArea:update(dt)
+  self.groupMenu:update(dt)
+  self.layerMenu:update(dt)
 end
 
 function LayerPicker:draw()
-  self.scrollArea:draw()
+  self.groupMenu:draw()
+  self.layerMenu:draw()
 end
 
 return LayerPicker
