229f5cf58294fef1a9fb080096f78867d130bf72
tesselode <aminnich3@gmail.com>
2015-11-17 14:40:55 -0500

    Cursor

--- state/editor.lua
+++ state/editor.lua
@@ -1,4 +1,5 @@
 local lg = love.graphics
+local lm = love.mouse
 
 local function lerp(a, b, x)
   return a + (b - a) * x
@@ -10,10 +11,23 @@ function Editor:enter()
   self.mousePos  = Vector()
   self.mousePrev = Vector()
 
-  self.pan          = Vector()
-  self.scale        = 5
+  self.pan          = Vector(lg.getWidth() / 2, lg.getHeight() / 2)
+  self.scale        = 25
   self.displayPan   = self.pan
   self.displayScale = self.scale
+  self.cursor       = Vector()
+end
+
+function Editor:getRelativeMousePos()
+  local pos = Vector()
+  pos.x = (lm.getX() - self.displayPan.x) / self.scale + level.width / 2
+  pos.y = (lm.getY() - self.displayPan.y) / self.scale + level.height / 2
+  return pos
+end
+
+function Editor:getCursorWithinMap()
+  local c = self.cursor
+  return c.x >= 0 and c.x < level.width and c.y >= 0 and c.y < level.height
 end
 
 function Editor:update(dt)
@@ -31,6 +45,11 @@ function Editor:update(dt)
   self.displayPan.x = lerp(self.displayPan.x, self.pan.x, 1 - (10^-5) ^ dt)
   self.displayPan.y = lerp(self.displayPan.y, self.pan.y, 1 - (10^-5) ^ dt)
   self.displayScale = lerp(self.displayScale, self.scale, 1 - (10^-5) ^ dt)
+
+  --cursor
+  local relativeMousePos = self:getRelativeMousePos()
+  self.cursor.x          = math.floor(relativeMousePos.x)
+  self.cursor.y          = math.floor(relativeMousePos.y)
 end
 
 function Editor:mousepressed(x, y, button)
@@ -44,30 +63,37 @@ end
 
 function Editor:draw()
   lg.push()
-  lg.translate(lg.getWidth() / 2, lg.getHeight() / 2)
   lg.translate(self.displayPan.x, self.displayPan.y)
   lg.scale(self.displayScale)
   lg.translate(-level.width / 2, -level.height / 2)
 
-  love.graphics.setLineWidth(1 / self.displayScale)
+    love.graphics.setLineWidth(1 / self.displayScale)
 
-  --draw border
-  love.graphics.setColor(255, 255, 255)
-  lg.line(0, 0, level.width, 0)
-  lg.line(0, level.height, level.width, level.height)
-  lg.line(0, 0, 0, level.height)
-  lg.line(level.width, 0, level.width, level.height)
+    --draw border
+    love.graphics.setColor(255, 255, 255)
+    lg.line(0, 0, level.width, 0)
+    lg.line(0, level.height, level.width, level.height)
+    lg.line(0, 0, 0, level.height)
+    lg.line(level.width, 0, level.width, level.height)
 
-  --draw gridlines
-  lg.setColor(255, 255, 255, 100)
-  for i = 1, level.width - 1 do
-    lg.line(i, 0, i, level.height)
-  end
-  for i = 1, level.height - 1 do
-    lg.line(0, i, level.width, i)
-  end
+    --draw gridlines
+    lg.setColor(255, 255, 255, 100)
+    for i = 1, level.width - 1 do
+      lg.line(i, 0, i, level.height)
+    end
+    for i = 1, level.height - 1 do
+      lg.line(0, i, level.width, i)
+    end
+
+    --draw cursor
+    if self:getCursorWithinMap() then
+      lg.rectangle('fill', self.cursor.x, self.cursor.y, 1, 1)
+    end
 
   lg.pop()
+
+  lg.setColor(255, 255, 255)
+  lg.print(self.cursor.x..' '..self.cursor.y)
 end
 
 return Editor
