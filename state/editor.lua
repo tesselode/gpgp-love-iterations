f02d9a168b1e6b9857f3e1f6ec17533b48f8e9d0
tesselode <aminnich3@gmail.com>
2015-11-18 00:18:13 -0500

    Made a grid class

--- state/editor.lua
+++ state/editor.lua
@@ -1,83 +1,39 @@
 local lg = love.graphics
 local lm = love.mouse
 
-local function lerp(a, b, x)
-  return a + (b - a) * x
-end
+local Grid = require 'ui.grid'
 
 local Editor = {}
 
 function Editor:enter()
-  self.mousePos  = Vector()
-  self.mousePrev = Vector()
-
-  self.pan            = Vector(lg.getWidth() / 2, lg.getHeight() / 2)
-  self.scale          = 25
-  self.displayPan     = self.pan
-  self.displayScale   = self.scale
-  self.cursor         = Vector()
-
+  self.grid           = Grid()
   self.selectedGroup  = Project.level.groups[1]
   self.selectedLayer  = self.selectedGroup.layers[1]
   self.selectedEntity = Project.entities[1]
 end
 
-function Editor:getRelativeMousePos()
-  local pos   = Vector()
-  local level = Project.level
-  pos.x = (lm.getX() - self.displayPan.x) / self.scale + level.width / 2
-  pos.y = (lm.getY() - self.displayPan.y) / self.scale + level.height / 2
-  return pos
-end
-
-function Editor:getCursorWithinMap()
-  local c = self.cursor
-  return c.x >= 0
-     and c.x < Project.level.width
-     and c.y >= 0
-     and c.y < Project.level.height
-end
-
 function Editor:place()
   self:remove()
   table.insert(self.selectedLayer.entities, {
-    x    = self.cursor.x,
-    y    = self.cursor.y,
+    x    = self.grid.cursor.x,
+    y    = self.grid.cursor.y,
     name = self.selectedEntity.name,
   })
 end
 
 function Editor:remove()
   for n, entity in pairs(self.selectedLayer.entities) do
-    if entity.x == self.cursor.x and entity.y == self.cursor.y then
+    if entity.x == self.grid.cursor.x and entity.y == self.grid.cursor.y then
       table.remove(self.selectedLayer.entities, n)
     end
   end
 end
 
 function Editor:update(dt)
-  --track mouse movement
-  self.mousePrev = self.mousePos
-  self.mousePos  = Vector(love.mouse.getX(), love.mouse.getY())
-  local mouseDelta = self.mousePos - self.mousePrev
-
-  --panning
-  if love.mouse.isDown 'm' then
-    self.pan        = self.pan + mouseDelta
-    self.displayPan = self.pan
-  end
-
-  self.displayPan.x = lerp(self.displayPan.x, self.pan.x, 1 - (10^-5) ^ dt)
-  self.displayPan.y = lerp(self.displayPan.y, self.pan.y, 1 - (10^-5) ^ dt)
-  self.displayScale = lerp(self.displayScale, self.scale, 1 - (10^-5) ^ dt)
-
-  --cursor
-  local relativeMousePos = self:getRelativeMousePos()
-  self.cursor.x          = math.floor(relativeMousePos.x)
-  self.cursor.y          = math.floor(relativeMousePos.y)
+  self.grid:update(dt)
 
   --place objects
-  if self:getCursorWithinMap() then
+  if self.grid:getCursorWithinMap() then
     if love.mouse.isDown('l') then
       self:place()
     end
@@ -88,37 +44,22 @@ function Editor:update(dt)
 end
 
 function Editor:mousepressed(x, y, button)
-  if button == 'wu' then
-    self.scale = self.scale * 1.1
-  end
-  if button == 'wd' then
-    self.scale = self.scale / 1.1
-  end
+  self.grid:mousepressed(x, y, button)
+end
+
+function Editor:drawCursorImage()
+  local i  = self.selectedEntity.image
+  local x  = self.grid.cursor.x
+  local y  = self.grid.cursor.y
+  local sx = (self.selectedEntity.width or 1) / i:getWidth()
+  local sy = (self.selectedEntity.height or 1) / i:getHeight()
+  lg.draw(i, x, y, 0, sx, sy)
 end
 
 function Editor:draw()
-  lg.push()
-  lg.translate(self.displayPan.x, self.displayPan.y)
-  lg.scale(self.displayScale)
-  lg.translate(-Project.level.width / 2, -Project.level.height / 2)
-
-    love.graphics.setLineWidth(1 / self.displayScale)
-
-    --draw border
-    love.graphics.setColor(255, 255, 255)
-    lg.line(0, 0, Project.level.width, 0)
-    lg.line(0, Project.level.height, Project.level.width, Project.level.height)
-    lg.line(0, 0, 0, Project.level.height)
-    lg.line(Project.level.width, 0, Project.level.width, Project.level.height)
-
-    --draw gridlines
-    lg.setColor(255, 255, 255, 100)
-    for i = 1, Project.level.width - 1 do
-      lg.line(i, 0, i, Project.level.height)
-    end
-    for i = 1, Project.level.height - 1 do
-      lg.line(0, i, Project.level.width, i)
-    end
+  self.grid:drawTransformed(function()
+    self.grid:drawBorder()
+    self.grid:drawGrid()
 
     --draw layers
     lg.setColor(255, 255, 255)
@@ -127,7 +68,7 @@ function Editor:draw()
         for _, entity in pairs(layer.entities) do
           --find the entity to draw
           local e
-          for _, entityDef in pairs(entities) do
+          for _, entityDef in pairs(Project.entities) do
             if entityDef.name == entity.name then
               e = entityDef
               break
@@ -145,23 +86,9 @@ function Editor:draw()
       end
     end
 
-    --draw cursor
-    if self:getCursorWithinMap() then
-      lg.setColor(255, 255, 255, 100)
-      lg.rectangle('fill', self.cursor.x, self.cursor.y, 1, 1)
-
-      local i  = self.selectedEntity.image
-      local x  = self.cursor.x
-      local y  = self.cursor.y
-      local sx = (self.selectedEntity.width or 1) / i:getWidth()
-      local sy = (self.selectedEntity.height or 1) / i:getHeight()
-      lg.draw(i, x, y, 0, sx, sy)
-    end
-
-  lg.pop()
-
-  lg.setColor(255, 255, 255)
-  lg.print(self.cursor.x..' '..self.cursor.y)
+    self.grid:drawCursor()
+    self:drawCursorImage()
+  end)
 end
 
 return Editor
