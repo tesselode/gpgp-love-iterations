5c178b79579519cc2ae72e78c5cd820331d8a05e
tesselode <aminnich3@gmail.com>
2015-11-20 13:03:56 -0500

    Tweens + reverting glowy buttons for now

--- state/entity-palette.lua
+++ state/entity-palette.lua
@@ -10,31 +10,32 @@ function EntityButton:new(x, y, w, h, entity, layer)
   self.entity = entity
   self.layer = layer
 
-  self.color = {
+  --[[self.color = {
     normal  = {0, 0, 0, 0},
     hover   = {0, 0, 0, 0},
     pressed = {0, 0, 0, 0},
-  }
+  }]]
 
   --cosmetic
-  self.glow = love.graphics.newCanvas(bSize, bSize)
-  self.glow:renderTo(function()
-    local whiteShader = love.graphics.newShader[[
-      vec4 effect(vec4 color, sampler2D texture, vec2 uv, vec2 sc)
-      {
-        return vec4(1.0, 1.0, 1.0, texture2D(texture, uv).a) * color;
-      }
-    ]]
+  --self.glow = love.graphics.newCanvas(bSize, bSize)
+  --self.glow:renderTo(function()
+    --local whiteShader = love.graphics.newShader[[
+      --vec4 effect(vec4 color, sampler2D texture, vec2 uv, vec2 sc)
+      --{
+        --return vec4(1.0, 1.0, 1.0, texture2D(texture, uv).a) * color;
+      --}
+    --]]
 
-    local gaussianblur      = require('lib.shine').gaussianblur()
-    gaussianblur.parameters = {sigma = 16}
+    --local gaussianblur      = require('lib.shine').gaussianblur()
+    --gaussianblur.parameters = {sigma = 16}
 
-    gaussianblur:draw(function()
-      love.graphics.setShader(whiteShader)
-      self:drawImage(bSize / 2, bSize / 2)
-      love.graphics.setShader()
-    end)
-  end)
+    --gaussianblur:draw(function()
+      --love.graphics.setShader(whiteShader)
+      --love.graphics.setColor(255, 255, 255)
+      --self:drawImage(bSize / 2, bSize / 2)
+      --love.graphics.setShader()
+    --end)
+  --end)
 end
 
 function EntityButton:onPressed()
@@ -42,7 +43,7 @@ function EntityButton:onPressed()
   require('lib.gamestate').pop()
 end
 
-function EntityButton:drawImage(x, y, cs)
+function EntityButton:drawImage(x, y)
   cs = cs or 1
 
   if self.entity.image then
@@ -57,21 +58,21 @@ function EntityButton:drawImage(x, y, cs)
     else
       scale = self.h / image:getHeight() * .5
     end
-    love.graphics.draw(image, x, y, 0, scale * cs, scale * cs, ox, oy)
+    love.graphics.draw(image, x, y, 0, scale, scale, ox, oy)
   end
 end
 
 function EntityButton:draw()
   EntityButton.super.draw(self)
 
-  if self.mouseOver then
+  --[[if self.mouseOver then
     love.graphics.setColor(255, 255, 255)
     love.graphics.setBlendMode('additive')
     for i = 1, 3 do
       love.graphics.draw(self.glow, self.x, self.y)
     end
     love.graphics.setBlendMode('alpha')
-  end
+  end]]
 
   love.graphics.setColor(255, 255, 255)
   self:drawImage(self:getCenter())
@@ -89,6 +90,12 @@ function EntityPalette:enter(previous, layer)
   self.previous = previous
   self.layer    = layer
   self:generateMenu()
+
+  --cosmetic
+  self.canvasAlpha = 0
+  self.canvasY     = 100
+  self.tween       = require('lib.flux').group()
+  self.tween:to(self, .25, {canvasY = 0, canvasAlpha = 255})
 end
 
 function EntityPalette:generateMenu()
@@ -120,6 +127,7 @@ function EntityPalette:generateMenu()
   end
 
   --cosmetic
+  self.canvas     = love.graphics.newCanvas()
   self.background = love.graphics.newCanvas()
   self.background:clear(Color.AlmostBlack)
   self.background:renderTo(function()
@@ -142,13 +150,21 @@ function EntityPalette:resize()
 end
 
 function EntityPalette:update(dt)
+  self.tween:update(dt)
   self.scrollArea:update(dt)
 end
 
 function EntityPalette:draw()
   love.graphics.setColor(150, 150, 150)
   love.graphics.draw(self.background)
-  self.scrollArea:draw(dt)
+
+  self.canvas:clear(0, 0, 0, 0)
+  self.canvas:renderTo(function()
+    self.scrollArea:draw(dt)
+  end)
+
+  love.graphics.setColor(255, 255, 255, self.canvasAlpha)
+  love.graphics.draw(self.canvas, 0, self.canvasY)
 end
 
 return EntityPalette
