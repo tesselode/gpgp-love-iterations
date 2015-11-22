9258722b4bbcedcbaf0afb55ddd6f2547f8631d5
tesselode <aminnich3@gmail.com>
2015-11-22 15:13:33 -0500

    Message display system

--- main.lua
+++ main.lua
@@ -1,12 +1,16 @@
-Class  = require 'lib.classic'
-Vector = require 'lib.vector'
+Class        = require 'lib.classic'
+Vector       = require 'lib.vector'
+conversation = require('lib.talkback').new()
 require 'colors'
 require 'fonts'
 
-local messageText  = ''
-local messageAlpha = 0
-local timer        = require('lib.timer').new()
-local tween        = require('lib.flux').group()
+local message = {text = '', time = 0, alpha = 0}
+
+conversation:listen('displayMessage', function(toDisplay)
+  message.text = toDisplay
+  message.time = 0
+  message.alpha = 255
+end)
 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -18,12 +22,25 @@ function love.load()
 end
 
 function love.update(dt)
-  timer.update(dt)
-  tween:update(dt)
+  message.time = message.time + dt
+  if message.time > 2 then
+    message.alpha = message.alpha - 255 * dt
+    if message.alpha < 0 then
+      message.alpha = 0
+    end
+  end
+end
+
+function love.keypressed(key)
+  if key == 's' then
+    conversation:say('displayMessage', 'hello world!')
+  end
 end
 
 function love.draw()
   local c = Color.AlmostWhite
-  love.graphics.setColor(c[1], c[2], c[3], messageAlpha)
-  love.graphics.print(messageText)
+  love.graphics.setColor(c[1], c[2], c[3], message.alpha)
+  love.graphics.setFont(Font.Medium)
+  local y = love.graphics.getHeight() - Font.Medium:getHeight('test')
+  love.graphics.print(message.text, 5, y - 5)
 end
