1d506f53fea579fc75ae1cea2914854124fe639e
Andrew Minnich <aminnich3@gmail.com>
2018-08-18 14:54:10 -0400

    placeholder functions

--- source/class/layer/layer.lua
+++ source/class/layer/layer.lua
@@ -7,6 +7,11 @@ function Layer:new(map, name)
 	self.name = name
 end
 
+function Layer:place(x, y, item) end
+function Layer:remove(x, y) end
+function Layer:save() end
+function Layer:load(data) end
+function Layer:drawCursor(x, y, item) end
 function Layer:draw() end
 
 return Layer
