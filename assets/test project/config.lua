325c530d324c617828910f2b789affc9dd0fd2ae
Andrew Minnich <aminnich3@gmail.com>
2018-12-18 01:02:09 -0500

    tileset definition format

--- assets/test project/config.lua
+++ assets/test project/config.lua
@@ -1,3 +1,25 @@
 return {
 	defaultLevelWidth = 32,
+	tilesets = {
+		{
+			name = 'Fall',
+			tileSize = 16,
+			imagePath = 'fall.png'
+		},
+		{
+			name = 'Grass',
+			tileSize = 16,
+			imagePath = 'grass.png'
+		},
+		{
+			name = 'Tropical',
+			tileSize = 16,
+			imagePath = 'tropical.png'
+		},
+		{
+			name = 'Winter',
+			tileSize = 16,
+			imagePath = 'winter.png'
+		},
+	}
 }
